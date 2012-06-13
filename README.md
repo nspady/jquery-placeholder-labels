eb9bf790d935c94ed5febcb803aba9d0964a0dd5
Nathan Spady <nspady@gmail.com>
2012-06-13 11:26:31 -0700

    Added the primary files and improved the documentation

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
 jquery-placeholder-labels
 =========================
 
-A jQuery plugin to mimic Chrome and Safari's handeling of the placeholder attribute for unsupported browsers.
+A jQuery plugin to mimic Chrome and Safari's handeling of the placeholder for unsupported browsers. The plugin functions by adding a label element on top of the input element. This way the placeholder text remains visable untill a character is entered.
+
+Setup
+-----
+Required componenets for the plugin to function
+
+### CSS
+`/* Class added to placeholder text on focus */
+.input-selected {
+	color: #aaa !important;
+	}
+
+/* Styling of the placeholder */
+.placeholder span {
+  position: absolute;
+  padding: 5px; /* Set to match your input styling */
+  color: #999; /* Color of the placeholder text */
+  }`
+
+### JS
+Include after jQuery is loaded
+`$( function() {
+    add_placeholders();
+	});
+`
+
+### Example label and text area
+`<input type="text" name="name" placeholder="e.g. John Smith"></label>`
+
+`<textarea name="comment" placeholder="Your comment goes here"></textarea>`
+
+
+Browser support
+---------------
+Tested successfully on:
+IE 7, 8, and 9
+
+
---
+++ placeholder-labels.js
@@ -0,0 +1,92 @@
+function add_placeholders() {// encapsulate our variables into their own scope  
+    // test if HTML5 placeholder is supported; if so, quit  
+    if ('placeholder' in document.createElement('input')) {
+      return;  
+    }
+  
+    /* Go through each input and text area and add a placeholder label */
+    $("input[placeholder], textarea[placeholder]").each(function(){  
+      var $input = $(this);
+      var placeholder_text = $input.attr('placeholder'); 
+  
+      // create a label and a span within it  
+      var $label = $('<label class="placeholder"></label>')
+      var $span = $('<span>' + placeholder_text + '</span>');
+      $span.appendTo($label);  
+  
+      // the label surrounds the input  
+      $label.insertBefore($input).append($input);  
+  
+      // try getting the input's size  
+      var w = $input.width(),  
+          h = $input.height();  
+  
+      // test for the case of the input being hidden
+      if (!w || !h) {  
+        // get dimensions by cloning the input and grabbing height and width  
+        var $clone = $input.clone().appendTo('body').css({  
+          position : 'absolute',  
+          visibility : 'hidden',  
+          display : 'block'  
+        });  
+  
+        // fetch the correct size (hopefully)  
+        w = $clone.width();  
+        h = $clone.height();  
+  
+        $clone.remove();  
+      }  
+ 
+      // if the input is hidden or shown, so should the placeholder be 
+      $input.bind('hide', function(){ $span.hide(); }); 
+      // when showing, ensure the text is the right size 
+      $input.bind('show', function(){ 
+        $span.show().css({ 
+          width:$input.width()+'px', 
+          height:$input.height()+'px' 
+        }); 
+      }); 
+ 
+      /* If the input is starting out hidden or there is a default value in 
+       * the input already, hide the placeholder 
+       */ 
+      if (!$input.is(':visible') || $input.val().length) {
+        $span.hide(); 
+      }
+ 
+      /* Show the placeholder untill a key is pressed in the field
+       * restore the placeholder if the field is blank.
+       */
+      $input
+        .bind("keydown keyup", function(){ 
+          if ( $(this).val() == "") {
+            $span.show();
+          } else {
+            $span.hide();
+          }
+        })
+        .focus( function() {
+          $span.addClass("input-selected");
+        })
+        .blur( function() {
+          $span.removeClass("input-selected");
+        });
+ 
+      // If the placeholder is focused, send focus to the input 
+      $span.focus(function(){ $input.focus(); }); 
+    }); 
+ 
+    // override jQuery.hide() to trigger the 'hide' event 
+    var hide = $.fn.hide; 
+    $.fn.hide = function() { 
+      $(this).trigger('hide'); 
+      return hide.apply(this, arguments); 
+    }; 
+ 
+    // override jQuery.show() to trigger the 'show' event 
+    var show = $.fn.show; 
+    $.fn.show = function() { 
+      $(this).trigger('show');  
+      return show.apply(this, arguments);  
+    };  
+  };  
