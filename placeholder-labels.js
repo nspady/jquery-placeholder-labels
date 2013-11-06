cf5aff98a8bf43156de149e2e06c4b4910e21e62
Nathan Spady <nspady@gmail.com>
2013-11-06 13:27:56 -0800

    Updated to override IE10-11 default placeholder behvior and copy styles from inputs and textboxes

--- placeholder-labels.js
+++ placeholder-labels.js
@@ -1,16 +1,30 @@
 /* Author: Nathan Spady
  *
  * Add placeholder attribute support for older browsers such as IE 7-9
- * 
+ * Also overrides IE 10-11 default behavior which hides the placeholder on focus
+ *
  * https://github.com/nspady/jquery-placeholder-labels
  */
 
 ( function( $ ) {
-  $.fn.placeholder_labels = function() {
-    // encapsulate our variables into their own scope
-    // test if HTML5 placeholder is supported; if so, quit
-    if ('placeholder' in document.createElement('input')) {
-      return ;
+  var IE10 = !!(navigator && navigator.userAgent && navigator.userAgent.match('MSIE 10'));
+  var IE11 = !!(navigator && navigator.userAgent && navigator.userAgent.match('MSIE 11'));
+  var overridePlaceholder = (IE10 || IE11);
+
+  $.fn.placeholder_labels = function(options) {
+    var that = this;
+    var defaults = {
+      blurColor  : '#aaaaaa',
+      focusColor : '#bbbbbb',
+      labelClass : 'placeholder-label'
+    }
+
+    var settings = $.extend({}, defaults, options);
+
+    // test if HTML5 placeholder is supported or we do not want to
+    // override default placeholder behavior; if so, quit
+    if ('placeholder' in document.createElement('input') && !overridePlaceholder) {
+      return this;
     }
 
     // Go through each input and/or text area and add a placeholder label
@@ -20,15 +34,50 @@
       var $input = $( this );
       var placeholder_text = $input.attr( 'placeholder' );
 
+      if (overridePlaceholder) $input.attr('placeholder', '');
+
+      var line_height = {};
+      if( !$input.is('textarea') && $input.css('height') != 'auto') {
+        line_height = { lineHeight: $input.css('height'), whiteSpace: 'nowrap' };
+      }
+
       // create a label and a span within it
-      var $label = $( '<label class="placeholder"></label>' );
+      var $label = $( '<label></label>' ).addClass(settings.labelClass);
+
       var $span = $( '<span>' + placeholder_text + '</span>' );
       $span.appendTo( $label );
+
+      // Copy input styles and apply them to the placeholder
+      // should get it 95% of the way toward looking right
+      $span.css($.extend({
+        position:'absolute',
+        display: 'inline',
+        float:' none',
+        overflow: 'hidden',
+        textAlign: 'left',
+        color: settings.blurColor,
+        cursor: 'text',
+        paddingTop: $input.css('padding-top'),
+        paddingRight: $input.css('padding-right'),
+        paddingBottom: $input.css('padding-bottom'),
+        paddingLeft: $input.css('padding-left'),
+        marginTop: $input.css('margin-top'),
+        marginRight: $input.css('margin-right'),
+        marginBottom: $input.css('margin-bottom'),
+        marginLeft: $input.css('margin-left'),
+        fontSize: $input.css('font-size'),
+        fontFamily: $input.css('font-family'),
+        fontStyle: $input.css('font-style'),
+        fontWeight: $input.css('font-weight'),
+        textTransform: $input.css('text-transform'),
+        backgroundColor: 'transparent',
+        zIndex: 99
+      }, line_height));
 
       // the label surrounds the input
       $label.insertBefore( $input ).append( $input );
 
-      // try getting the input's size
+      // try getting the input's size even if it is hidden
       var w = $input.width(),
           h = $input.height();
 
@@ -49,11 +98,11 @@
       }
 
       // if the input is hidden or shown, so should the placeholder be
-      $input.bind('hide', function() {
+      $input.on('hide', function() {
         $span.hide();
       });
       // when showing, ensure the text is the right size
-      $input.bind('show', function(){
+      $input.on('show', function(){
         $span.show().css({
           width:$input.width()+'px',
           height:$input.height()+'px'
@@ -71,7 +120,7 @@
        * restore the placeholder if the field is blank.
        */
       $input
-        .bind("keydown keyup", function() {
+        .on("keydown keyup", function() {
           if ( $(this).val() === "" ) {
             $span.show();
           } else {
@@ -79,10 +128,14 @@
           }
         })
         .focus( function() {
-          $span.addClass( "input-selected" );
+          $span
+            .addClass( "input-selected" )
+            .css('color', settings.focusColor);
         })
         .blur( function() {
-          $span.removeClass( "input-selected" );
+          $span
+            .removeClass( "input-selected" )
+            .css('color', settings.blurColor);
         });
 
       // If the placeholder is focused, send focus to the input
@@ -104,5 +157,7 @@
       this.trigger('show');
       return show.apply(this, arguments);
     };
+
+    return that;
   };
 })( jQuery );
